cc15c8d339558ea2a8c2039c391ed17aee9c7e2a
mrdjangodev <mirolimcoder@gmail.com>
2024-03-06 17:36:44 +0500

    University and Faculty models have been created

--- university/models.py
+++ university/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class University(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    image = models.ImageField(upload_to='university')
+    location = models.CharField(max_length=250)
+    location_link = models.CharField(max_length=250, blank=True)
+    joined_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-joined_at']
+
+    def __str__(self):
+        return self.name
+    
+    def get_all_ttj(self):
+        pass
+
+
+    def get_all_students_placed_in_ttj(self):
+        pass
+
+    
+
+class Faculty(models.Model):
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
+
