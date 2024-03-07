847cd0b04608e8913a6ea641426d5bb7ba11660f
mrdjangodev <mirolimcoder@gmail.com>
2024-03-08 00:43:51 +0500

    University app's all models displayed on AdminPanel

--- university/admin.py
+++ university/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+# from local apps
+from .models import University, Faculty
 # Register your models here.
+
+
+class UnivesityAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'location', 'location_link', 'joined_at']
+    search_fields = ['name', 'location']
+admin.site.register(University, UnivesityAdmin)
+
+
+class FacultyAdmin(admin.ModelAdmin):
+    list_display = ['id', 'university', 'name']
+    search_fields = ['university__name', 'name']
+admin.site.register(Faculty, FacultyAdmin)
