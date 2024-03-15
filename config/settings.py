80715257989ced9a0fd82872d58e7aa29e5c648c
mrdjangodev <mirolimcoder@gmail.com>
2024-03-15 15:00:44 +0500

    Media_root and Media_url berildi

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,8 @@ LANGUAGES = [
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = 'media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
