91f08f935dc31dfff0efb67bfbd65beabc3c9e41
mrdjangodev <mirolimcoder@gmail.com>
2024-03-15 15:35:08 +0500

    Booking review uchun signal yozildi

--- student/signals.py
+++ student/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 
 from .models import (
-    BlackList, Student, Booking
+    BlackList, Student, Booking, BookingReview
 )
 
 
@@ -19,4 +19,15 @@ def change_student_approved_field_by_booking(sender, instance, **kwargs):
         student = instance.student
         student.approved = True
         student.save()
+
+    
+
+@receiver(post_save, sender=BookingReview)
+def update_booking_sattus_by_bookin_review(sender, instance, **kwargs):
+    booking = instance.booking
+    if instance.acceptance == 1:
+        booking.status = 2
+    else:
+        booking.status = 0
+    booking.save()
 
