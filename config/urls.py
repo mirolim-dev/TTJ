60fb213c8d4f2d75624b9f0cdae85ee6fc5bf1a1
mrdjangodev <mirolimcoder@gmail.com>
2024-04-29 20:56:28 +0500

    media and static urls addedt to main url patterns

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.i18n import set_language
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('i18n/', set_language, name='set_language'),
     path('student/', include('student.urls')),
 ]
+# Add static and media URL patterns
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
