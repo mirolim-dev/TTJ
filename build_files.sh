21a2d37ad6fc506be44abdbc07824cfd11e20dd2
mrdjangodev <mirolimcoder@gmail.com>
2024-05-07 00:01:10 +0500

    build.sh changed

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,6 @@
 echo " BUILD START"
 #!/bin/bash
-
-# Install pip
-curl https://bootstrap.pypa.io/get-pip.py | python3.9
-
-# Install project dependencies
-python3.9 -m pip install -r requirements.txt
+pip install -r requirements.txt
 
 python3.9 manage.py collectstatic
 echo "BUILD END"
