e84d8dac7ff333c2f2cb88b90e6652962963585a
mrdjangodev <mirolimcoder@gmail.com>
2024-05-07 20:54:23 +0500

    pip installed

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
+# Install pip
+curl https://bootstrap.pypa.io/get-pip.py | python3.9
+
+# Install project dependencies
 python3.9 -m pip install -r requirements.txt
+
 python3.9 manage.py collectstatic
